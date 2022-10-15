7ac111150e38aac9c6b54d1a6f20998b33df33ed
akshaychirme <akshaychirme29@gmail.com>
2022-10-15 15:24:12 +0530

    done

--- prog.java
+++ prog.java
@@ -2,5 +2,6 @@ public class prog {
 	public static void main(String[]args){
 		System.out.println("Hello world");
 		System.out.println("Helloworld");
+		System.out.println("My name is AKSHAY");
 	}
 }
